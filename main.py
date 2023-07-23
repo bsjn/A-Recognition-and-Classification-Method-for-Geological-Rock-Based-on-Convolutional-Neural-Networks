dcecae2d5f7b03b8d5b39504d8aa7460b91b97fa
陆晨旭 <1192316001@qq.com>
2023-07-23 20:42:05 +0800

    这是一个提交测试

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class MNISTLoader():
         mnist = tf.keras.datasets.mnist
         (self.train_data,self.train_label),(self.test_data,self.test_label) = mnist.load_data()
         # MNIST中的图像默认为uint8（0-255的数字）。以下代码将其归一化到0-1之间的浮点数，并在最后增加一维作为颜色通道RGB，如果没有这个维度就是灰度的图片，没有彩色。
-        self.train_data = np.expand_dims(self.train_data.astype(np.float)/255.0,axis=-1)  # [60000, 28, 28, 1]
+        self.train_data = np.expand_dims(self.train_data.astype(np.float)/255.0,axis=-1)            # [60000, 28, 28, 1]
         self.test_data = np.expand_dims(self.test_data.astype(np.float32) / 255.0, axis=-1)        # [10000, 28, 28, 1]
         self.train_label = self.train_label.astype(np.int32)    # [60000]
         self.test_label = self.test_label.astype(np.int32)      # [10000]
